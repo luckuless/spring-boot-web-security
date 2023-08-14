278e83bb36bfdf450f5484de314390d5501f045f
luckuless <luckuless.rambojun@icloud.com>
2023-08-14 14:30:20 +0200

    angular setup

---
+++ security/src/main/java/be/luckuless/code/keycloak/SecurityApplication.java
@@ -0,0 +1,12 @@
+package be.luckuless.code.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
